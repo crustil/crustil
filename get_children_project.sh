3623b545c3d7d3c4069515917ce7d1d1af58c4f7
Teddy Fontaine <pro@teddyfontaine.fr>
2020-01-30 17:53:19 +0100

    add more options + update usage for get_children_project

--- get_children_project.sh
+++ get_children_project.sh
@@ -9,17 +9,30 @@ git_repository_path="micro-service/rx"
 usage() {
   echo -e "$0 <options>"
   echo ""
-  echo -e "\t--pom-path=<path>\t\tpom path"
-  echo -e "\t--pom-name=<path>\t\tpom name if differente of \"pom.xml\""
-  echo -e "\t--git-output=<path>\t\toutput directory with ../ (link to parent)"
+  echo -e "\t--pom-path=<path>\t\t\t\tpom path"
+  echo -e "\t--pom-name=<path>\t\t\t\tpom name if differente of \"pom.xml\""
+  echo -e "\t--git-output=<path>\t\t\t\toutput directory with ../ (link to parent)"
   echo ""
-  echo -e "\t--pull\t\t\t\tpull git repository"
-  echo -e "\t--clone\t\t\t\tclone git repository"
+  echo -e "\t--git-repository-path=<ssh://git@url:port>\tremote repository url + protocol"
+  echo -e "\t--ssh-prefix=<projetct_remote_path>\t\tremote project path"
+  echo ""
+  echo -e "\t--pull\t\t\t\t\t\tpull git repository"
+  echo -e "\t--clone\t\t\t\t\t\tclone git repository"
 }
 
 if [[ " $@ " == *" --help "* || " $@ " == *" -h "* ]]; then
   usage
   exit 0
+fi
+
+if [[ " $@ " =~ --ssh-prefix=([^' ']+) ]]; then
+  echo "+ set ssh prefix"
+  ssh_prefix=${BASH_REMATCH[1]}
+fi
+
+if [[ " $@ " =~ --git-repository-path=([^' ']+) ]]; then
+  echo "+ set git repository path"
+  git_repository_path=${BASH_REMATCH[1]}
 fi
 
 if [[ " $@ " =~ --pom-path=([^' ']+) ]]; then
@@ -38,7 +51,7 @@ if [[ " $@ " =~ --git-output=([^' ']+) ]]; then
   git_path_output="../${BASH_REMATCH[1]}"
 fi
 
-modules=$(grep -oP '.*\K(?<=>).*(?=<\/module)' pom.xml)
+modules=$(grep -oP '.*\K(?<=>).*(?=<\/module>)' pom.xml)
 modules=${modules//..\//}
 
 cmd() {
@@ -60,6 +73,10 @@ clone() {
 
 if [[ " $@ " == *" --clone "* ]]; then
   clone
+fi
+
+if [[ " $@ " == *" --fetch "* ]]; then
+  cmd fetch
 fi
 
 if [[ " $@ " == *" --pull "* ]]; then
