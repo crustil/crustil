b6144bf4ce1a75f75a8763efaac7625e30569e6a
Teddy Fontaine <pro@teddyfontaine.fr>
2020-07-09 11:37:00 +0200

    fix memory capacity

--- java/Dockerfile.java
+++ java/Dockerfile.java
@@ -7,11 +7,11 @@ ARG SERVICE=service
 ENV VERTICLE_FILE="./target/$SERVICE.jar" \
     VERTICLE_HOME="/opt/verticles" \
     VERTICLE_SERVICE=$SERVICE \
-    VERTICLE_JAVA_XMS="2m" \
-    VERTICLE_JAVA_XMX="64m" \
+    VERTICLE_JAVA_XMS="24m" \
+    VERTICLE_JAVA_XMX="256m" \
     VERTICLE_JAVA_MAX_PERM_SIZE="48m" \
     VERTICLE_JAVA_RESERVED_CODE_CACHE_SIZE="48m" \
-    VERTICLE_JAVA_MAX_RAM="128m"
+    VERTICLE_JAVA_MAX_RAM="320m"
 
 RUN cp /usr/share/zoneinfo/Europe/Paris /etc/localtime \
     && echo "Europe/Paris" > /etc/timezone
